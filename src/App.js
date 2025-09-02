f8f6c04b3a4ab7d7abb295348db9ed1d39347021
allyavorsky <allyavorsky@proton.me>
2025-09-02 15:14:11 +0300

    HW41: initial project setup with CRA and cleanup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Homework 41 | Розробка з використанням хука useEffect і Axios</h1>
+    </div>
+  );
+}
+
+export default App;
